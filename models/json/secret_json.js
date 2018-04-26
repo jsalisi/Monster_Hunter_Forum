2c1e01940e0471858f5cf4437efee940fba76181
RaziqK <raziqkhan7615@gmail.com>
2018-04-26 16:32:25 -0700

    changed post styles and thread styles

---
+++ models/json/secret_json.js
@@ -0,0 +1,7 @@
+{
+    "dbhost": "mysql-db-instance.cmnwaqammgar.us-west-2.rds.amazonaws.com",
+    "dbname": "monster_hunter_forum_DB",
+    "dbuser": "admin",
+    "dbpassword": "Passw0rd",
+    "port": "3306"
+}
