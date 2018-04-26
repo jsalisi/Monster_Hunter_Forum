f45eb20b55632c12a750d172cc6bbeb9cd0cc029
jsalisi <jsalisi@gmail.com>
2018-04-26 14:37:52 -0700

    added new thread functions and usage example

--- models/amazon-db-test.js
+++ models/amazon-db-test.js
@@ -4,48 +4,55 @@
 
 var database = require('./amazon_db');
 
-// Static Variables
-var thread_id = 9;                                              // thread_id:    New threads will increment from the last thread ID
-var title = "How does this work?";                              // title:        Titles will be retrieved from a from submission using request.body.topTitle
+// Static Variables                                             // thread_id:    New threads will increment from the last thread ID
+var title = "I am potato";                                      // title:        Titles will be retrieved from a from submission using request.body.topTitle
 var view_count = 0;                                             // view_count:   New threads will be initialized with a view count of 0. Link clicks will increment view count.
 var link = "/" + title.replace(/ /g, "_").substring(0, 14);     // link:         Spaces in the title are replaced with underscores; limited to 15 chars
 var init_post_id = 1;                                           // init_post_id: Initial post ID starts at 1 for every new thread
 var username = "bmalamb";                                       // username:     Retrieved from the currently logged in user
-var post = "Testing my js function";                            // post:         Retrieved from form submission using request.body.topContent
+var post = "Testing my thread id increment";                    // post:         Retrieved from form submission using request.body.topContent
 
-// To be used in server.js
-// Function call format for creating a new thread
-// Threads have an initial post that accompany it on creation
-database.createThread(thread_id, title, view_count, link).then((result) => {
-    console.log('Adding new thread...');
-    console.log(result);
 
-    // Initial post
-    var timestamp = new Date();
-    return database.createPost(init_post_id, thread_id, username, timestamp, post)
-
-}).then((result) => {
-    console.log('Adding new post...');
-    console.log(result);
+// Getting last thread ID
+database.getNextThreadID().then((thread_id) => {
     
-    // Add site redirection here
+    // To be used in server.js
+    // Function call format for creating a new thread
+    // Threads have an initial post that accompany it on creation
+    database.createThread(thread_id, title, view_count, link).then((result) => {
+        console.log('Adding new thread...');
+        console.log(result);
 
-    // Stops connection with the database
-    // Will also stop any functions after it
-    process.exit();
+        // Initial post
+        var timestamp = new Date();
+        return database.createPost(init_post_id, thread_id, username, timestamp, post)
+
+    }).then((result) => {
+        console.log('Adding new post...');
+        console.log(result);
+
+        // Add site redirection here
+
+        // Stops connection with the database
+        // Will also stop any functions after it
+        process.exit();
+    }).catch((error) => {
+        console.log(error);
+        process.exit();
+    });
 }).catch((error) => {
-    console.log(error);
+    console.log(error);    
     process.exit();
 });
 
 // Pulling information on Threads
-database.loadThreads().then((threads) => {
-    console.log(threads);   
-    process.exit(); 
-});
+// database.loadThreads().then((threads) => {
+//     console.log(threads);   
+//     process.exit(); 
+// });
 
 // Pulling information
-database.loadPosts().then((threads) => {
-    console.log(threads);
-    process.exit();
-});
+// database.loadPosts().then((threads) => {
+//     console.log(threads);
+//     process.exit();
+// });
--- models/amazon_db.js
+++ models/amazon_db.js
@@ -96,9 +96,25 @@ var createPost = (post_id, thread_id, username, datetime, post) => {
   });
 }
 
+var getNextThreadID = () => {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      // Use the connection
+      connection.query(`SELECT thread_id FROM monster_hunter_forum_DB.Threads ORDER BY thread_id DESC LIMIT 1;`, (error, results, fields) => {
+        // And done with the connection.
+        connection.release();
+        // Handle error after the release.
+        if (error) reject(error);
+        else resolve(results[0].thread_id + 1);
+      });
+    });
+  });
+}
+
 module.exports = {
   loadThreads,
   loadPosts,
   createThread,
-  createPost
+  createPost,
+  getNextThreadID
 }
