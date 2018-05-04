7d09d5a48ed1dcf3aca24fd57e45251626c4128a
boweichn <boweicheng@hotmail.com>
2018-05-04 16:05:39 -0700

    new register

--- public/js/register.js
+++ public/js/register.js
@@ -12,19 +12,37 @@ var user_check = false;
 var pass_check = false;
 var conf_check = false;
 
+var user_checkmark = document.getElementById("user_checkmark")
+var pass_checkmark = document.getElementById("pass_checkmark")
+var conf_checkmark = document.getElementById("conf_checkmark")
+
 var checkNewUser = () => {
     var user_value = new_user.value
 
-    if (user_value.length < 5) {
+    if (user_value.length < 5 && user_value.length >= 1) {
         user_error.innerHTML = 'Username has to have minimum of 5 characters!'
         user_check = false
+        user_checkmark.style.backgroundImage="url('/img/cross.png')";
+        user_checkmark.style.opacity = "1";
+
+    } else if (user_value.length == 0){
+        user_error.innerHTML = ''
+        user_check = false
+        user_checkmark.style.backgroundImage = "url('/img/cross.png')";
+        user_checkmark.style.opacity = "0";
+        
     } else if ((/^\w+$/).test(user_value) == false) {
         user_error.innerHTML = 'Username can only be composed of alphabet, underscores, and numbers!'
         user_check = false
+        user_checkmark.style.backgroundImage = "url('/img/cross.png')";
+        user_checkmark.style.opacity = "1";
+
     } else {
         user_error.innerHTML = ''
         user_check = true
-        
+        user_checkmark.style.backgroundImage = "url('/img/checkmark.png')";
+        user_checkmark.style.opacity="1";
+
     }
     console.log(user_check)
 }
@@ -32,12 +50,25 @@ var checkNewUser = () => {
 var checkPass = () => {
     var pass_value = new_pass.value
 
-    if (pass_value.length<8) {
+    if (pass_value.length<8 && pass_value.length >= 1) {
         pass_error.innerHTML = 'Passwords needs to be at least 8 characters in length!'
         pass_check = false
-    } else {
+        pass_checkmark.style.backgroundImage = "url('/img/cross.png')";
+        pass_checkmark.style.opacity = "1";
+
+    } else if (pass_value.length == 0) {
+        pass_error.innerHTML = ''
+        pass_check = false
+        pass_checkmark.style.backgroundImage = "url('/img/cross.png')";
+        pass_checkmark.style.opacity = "0";
+    } 
+    
+    else {
         pass_error.innerHTML = ''
         pass_check = true
+        pass_checkmark.style.opacity="1";
+        pass_checkmark.style.backgroundImage = "url('/img/checkmark.png')";
+
     }
     console.log(pass_check)
 }
@@ -46,13 +77,24 @@ var checkConf = () => {
     var conf_value = conf_pass.value
     var pass_value = new_pass.value
 
-    if (pass_value != conf_value) {
+    if (pass_value != conf_value && conf_value.length > 0) {
         conf_error.innerHTML = 'Confirmation does not match!'
         conf_check = false
+        conf_checkmark.style.backgroundImage = "url('/img/cross.png')";
+        conf_checkmark.style.opacity= "1";
+
+    } else if (pass_value == 0 || conf_value.length == 0){
+        conf_error.innerHTML = ''
+        conf_check = false
+        conf_checkmark.style.backgroundImage = "url('/img/checkmark.png')";
+        conf_checkmark.style.opacity = "0";
     } else {
         conf_error.innerHTML = ''
         conf_check = true
+        conf_checkmark.style.backgroundImage = "url('/img/checkmark.png')";
+        conf_checkmark.style.opacity = "1";
     }
+    
 }
 
 var activateButton = () => {
