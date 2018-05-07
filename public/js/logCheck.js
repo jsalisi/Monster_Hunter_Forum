f7933385acdc9d9b0f02c95b099b72cd2a21f373
boweichn <boweicheng@hotmail.com>
2018-05-07 10:46:32 -0700

    fixed spam login bug

--- public/js/logCheck.js
+++ public/js/logCheck.js
@@ -4,8 +4,9 @@ for (var key in localStorage) {
     currentUser = key;
 }
 
-var check = localStorage.getItem(currentUser)
-
-document.getElementById('logger').value = check;
-document.getElementById('userNow').value = currentUser;
-document.getElementById('checkForm').submit();
+setTimeout (() => {
+    var check = localStorage.getItem(currentUser)
+    document.getElementById('logger').value = check;
+    document.getElementById('userNow').value = currentUser;
+    document.getElementById('checkForm').submit();
+}, 50);
