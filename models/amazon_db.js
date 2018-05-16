6104692441dcc263fe42cc2329ebf6ae657c4e28
jsalisi <jsalisi@gmail.com>
2018-05-15 17:55:21 -0700

    reverting changes before using process.env

--- models/amazon_db.js
+++ models/amazon_db.js
@@ -1,15 +1,15 @@
 var mysql = require('mysql');
 
 // secret.json file is required for Amazon RDS to work
-//var config = require('./json/secret.json');
+var config = require('./json/secret.json');
 
 // Creates the connection to the database
 var pool  = mysql.createPool({
-  host: process.env.DBHOST,
-  user: process.env.DBUSER,
-  password: process.env.DBPASSWORD,
-  database: process.env.DBNAME,
-  port: process.env.DBPORT
+  host: config.dbhost,
+  user: config.dbuser,
+  password: config.dbpassword,
+  database: config.dbname,
+  port: config.port
 });
 
 /**
