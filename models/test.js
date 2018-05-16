046cc3e3e86ccb635063c58c3d9e2e78f87669d3
jsalisi <jsalisi@gmail.com>
2018-05-15 21:10:43 -0700

    refactoring tests

--- models/test.js
+++ models/test.js
@@ -1,45 +1,52 @@
 var db = require('./amazon-db-test.js');
 var database = require('./amazon_db.js');
 
-it('data contains correct properties in threads in Amazon RDS database', () => {
-    return database.loadThreads(0).then((threads) => {
-        for (var key in threads) {
-            expect(threads[`${key}`]).toHaveProperty('title');
-            expect(threads[`${key}`]).toHaveProperty('views');
-            expect(threads[`${key}`]).toHaveProperty('replies');
-            expect(threads[`${key}`]).toHaveProperty('started_by');
-            expect(threads[`${key}`]).toHaveProperty('post_date');
-            expect(threads[`${key}`]).toHaveProperty('last_poster');
-            expect(threads[`${key}`]).toHaveProperty('last_post_date');
-            expect(threads[`${key}`]).toHaveProperty('topic_link');
-        }
+describe('Thread tests from Amazon RDS database', () => {
+    test('thread properties', () => {
+        database.loadThreads(0).then((threads) => {
+            for (var key in threads) {
+                expect(threads[`${key}`]).toHaveProperty('title');
+                expect(threads[`${key}`]).toHaveProperty('views');
+                expect(threads[`${key}`]).toHaveProperty('replies');
+                expect(threads[`${key}`]).toHaveProperty('started_by');
+                expect(threads[`${key}`]).toHaveProperty('post_date');
+                expect(threads[`${key}`]).toHaveProperty('last_poster');
+                expect(threads[`${key}`]).toHaveProperty('last_post_date');
+                expect(threads[`${key}`]).toHaveProperty('topic_link');
+            }
+            process.exit();
+        });
+    });
+
+    test('thread result types', () => {
+        database.loadThreads(0).then((threads) => {
+            for (var i = 0; i < threads.length; i++) {
+                expect(typeof threads[i].title).toBe(string);
+                expect(typeof threads[i].views).toBe(number);
+                expect(typeof threads[i].replies).toBe(number);
+                expect(typeof threads[i].started_by).toBe(string);
+                expect(typeof threads[i].post_date).toBe(string);
+                expect(typeof threads[i].last_poster).toBe(string);
+                expect(typeof threads[i].last_post_date).toBe(string);
+                expect(typeof threads[i].topic_link).toBe(string);
+            }
+            process.exit();
+        });
     });
 });
 
-it('data contains correct property types in threads in Amazon RDS database', () => {
-    return database.loadThreads(0).then((threads) => {
-        for (var i = 0; i < threads.length; i++) {
-            expect(typeof threads[i].title).toBe(string);
-            expect(typeof threads[i].views).toBe(number);
-            expect(typeof threads[i].replies).toBe(number);
-            expect(typeof threads[i].started_by).toBe(string);
-            expect(typeof threads[i].post_date).toBe(string);
-            expect(typeof threads[i].last_poster).toBe(string);
-            expect(typeof threads[i].last_post_date).toBe(string);
-            expect(typeof threads[i].topic_link).toBe(string);
-        }
-    });
-});
-
-it('data contains correct post data properties in Amazon RDS database', async () => {
-    return database.loadPosts(14).then((data) => {
-        for (var i=0; i < data.length; i++) {
-            expect(data[i]).toHaveProperty("thread_id_fk")
-            expect(data[i]).toHaveProperty("post_id")
-            expect(data[i]).toHaveProperty("username")
-            expect(data[i]).toHaveProperty("post_date")
-            expect(data[i]).toHaveProperty("post")
-        }
+describe('Post tests from Amazon RDS database', async () => {
+    test("data contains correct post data properties", () => {
+        database.loadPosts(14).then((data) => {
+            for (var i = 0; i < data.length; i++) {
+                expect(data[i]).toHaveProperty("thread_id_fk")
+                expect(data[i]).toHaveProperty("post_id")
+                expect(data[i]).toHaveProperty("username")
+                expect(data[i]).toHaveProperty("post_date")
+                expect(data[i]).toHaveProperty("post")
+            }
+            process.exit();
+        });
     });
 })
 
